ccb196472538d5dd245ff6c6e2cce6ae67556578
Matheus Gomes <102872736+MatheusGomes23@users.noreply.github.com>
2023-03-03 00:33:12 -0300

    cadafunc

---
+++ app/cadastros/CadFuncionario.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.108.0">
+    <title>Checkout example · Bootstrap v5.3</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/checkout/">
+    <link href="../../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+    
+
+    
+    <!-- Custom styles for this template -->
+    <link href="checkout.css" rel="stylesheet">
+  </head>
+  <body class="bg-dark-subtle">
+    
+<div class="container">
+  <main>
+    <div class="py-5 text-center">
+      <img class="d-block mx-auto mb-4" src="../../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+      <h2>Cadastro de Funcionário</h2>
+    </div>
+
+    <div class="row g-5">     
+      <div class="col-md-7 col-lg-8">
+
+        <h4 class="mb-3">Dados Pessoais</h4>
+        <hr class="my-4">
+                    
+
+        <form class="needs-validation" novalidate> <!-- action="cad_Pessoabd.php" method="POST -->
+          <div class="row g-3">
+
+          <div class="col-sm-6">
+              <label for="codigo" class="form-label">Código</label>
+              <input type="text" class="form-control" id="codigo" name ="codigo" value="">
+            </div>
+  
+            <div class="col-sm-4">
+              <label for="codigo_pes" class="form-label">Código Pessoa</label>
+              <input type="text" class="form-control" id="codigo_pes" name ="codigo_pes" value="">
+            </div>
+
+            <div class="col-sm-8">
+              <label for="nome_pes" class="form-label">Nome Pessoa</label>
+              <input type="text" class="form-control" id="nome_pes" name ="nome_pes" value="">
+            </div>            
+
+            <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="prof">
+            <label class="form-check-label" for="save-info">Professor</label>
+          </div>
+
+            <div class="col-sm-6">
+              <label for="login" class="form-label">Login</label>
+              <input type="text" class="form-control" id="login" name="login" value="">
+            </div>
+
+            <div class="col-sm-6">
+              <label for="senha" class="form-label">Senha</label>
+              <input type="text" class="form-control" id="senha" name="senha" value="" required>     
+            </div>                            
+
+          <hr class="my-4">
+
+          <button class="w-100 btn btn-primary btn-lg" type="submit">Salvar</button>
+        </form>
+      </div>
+    </div>
+  </main>
+
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy; 2017–2022 Company Name</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
+</div>
+
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      <script src="checkout.js"></script>
+  </body>
+</html>
